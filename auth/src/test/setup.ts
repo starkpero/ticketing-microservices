6664a83e8abb4d264c2245953401b48fd62469ce
stark <anishgupta0066@gmail.com>
2022-11-30 11:11:00 +0530

    added global signin for testing to set the cookie in the follow up requests

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,19 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
+
+/*
+  to tell TypeScript about global signup function,
+  augment type definition
+*/
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -24,3 +37,21 @@ beforeAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+//Fixed the error by adding @types/node @15.14.1, because NodeJS.Global namespace was removed in @types/node 16.0.0 and @jest/types is broken
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("api/users/signup")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
