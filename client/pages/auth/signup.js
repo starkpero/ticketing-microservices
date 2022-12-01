72a56cda080b49f1eb9994fd3be434bc5d85f35a
stark <anishgupta0066@gmail.com>
2022-12-01 12:22:36 +0530

    deployment config for client, added bootstrap and signup form

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("/api/users/signup", {
+        email,
+        password,
+      });
+      console.log(response.data);
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign up</h1>
+      <div className="form-group">
+        <label>Email address</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form control"
+        ></input>
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="form control"
+        ></input>
+      </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Oops...</h4>
+          <ul className="my-0">
+            {errors.map((err) => {
+              <li key={err.message}>{err.message}</li>;
+            })}
+          </ul>
+        </div>
+      )}
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
