2d4c69f9def132375751bc0804155316ee9f8204
stark <anishgupta0066@gmail.com>
2022-12-05 10:48:49 +0530

    Header and Signout

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,30 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
+import Header from "../components/Header";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+
+  const { data } = await client.get("/api/users/currentuser");
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
